dcd578d541daf57e20f8ce8d124a52e3ecd4c9ad
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-04-23 23:12:44 -0600

    Build react-hook Users

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -4,7 +4,7 @@ import gql from 'graphql-tag';
 // import UserAvatar from './UserAvatar';
 // import CreateUser from './CreateUser';
 
-const GET_USER = gql `
+const GET_USERS = gql `
   {
     users {
       id
@@ -15,3 +15,21 @@ const GET_USER = gql `
     }
   }
 `;
+
+function Users({ selectUser }) {
+  const { loading, error, data } = useQuery(GET_USERS);
+
+  if (loading) return 'Loading...';
+  if (error) return `Error ${error.message}`;
+
+  return(
+    <div className="flex flex-wrap items-center pb-16">
+      {data.users.map(user => {
+        <div key={user.id} className="lg:w-1/3 w-full p-4 text-center inline" onClick="{selectUser.bind
+        (this, user)}">
+          <UserAvatar user={user} />
+        </div>
+      })}
+    </div>
+  )
+}
