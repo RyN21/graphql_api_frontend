86fef081b3fcbe38d3f9b0ae57f079a568c770c5
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-04-24 18:44:27 -0600

    Render User in App

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,22 @@
+import React from 'react'
+import Users from './Users';
 
+class App extends React.Component {
+  state = {
+    selectedUser: null
+  }
 
-function App() {
-  return (
-    <div className="App">
+  selectUser = (user) => {
+    this.setState({ selectUser: user })
+  }
 
-    </div>
-  );
+  render() {
+    return (
+      <div className="container mx-auto px-4">
+        {this.state.selectUser ? <User user={this.state.selectedUser} selectUser={this.selectUser}/> : <Users selectUser={this.selectUser}/>}
+      </div>
+    );
+  }
 }
 
 export default App;
--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
-// import UserAvatar from './UserAvatar';
+import UserAvatar from './UserAvatar';
 // import CreateUser from './CreateUser';
 
 const GET_USERS = gql `
