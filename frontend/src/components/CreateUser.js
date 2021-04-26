f4e91d8dbfc709f89d3dddf95ef4788da6f53ea8
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-04-25 21:06:31 -0600

    Finish render Mutation

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -29,4 +29,47 @@ class CreateUser extends Component {
     createUser({ variables: this.state });
     this.setState({ name: '', email: '', username: '' })
   }
+
+  render() {
+    return (
+      <Mutation
+        mutation={CREATE_USER}
+        update={this.props.onCreateUser}>
+          {createUserMutation => (
+            <div className="lg:fixed bottom-o left-0 w-full bg-white border-gray-300">
+              <form className="lg:px-8 pt-2 pb-2" onSubmit={e => this.onSubmit(e, createUserMutation)}>
+                <div className="lg:flex flex-wrap flex-between items-center justify-center lg:p-0 p-6">
+                  <h4 className="font-bold lg:pr-4 mb-2">Create new user</h4>
+                  <div className="lg:pr-4 mb-2">
+                    <input
+                      className="border rounded w-full py-2 px-3"
+                      type="text"
+                      value={this.state.name}
+                      placeholder="Name"
+                      onChange={e => this.setState({ name: e.target.value })} />
+                  </div>
+                  <div className="lg:pr-4 mb-2">
+                    <input
+                      className="border rounded w-full py-2 px-3"
+                      type="text"
+                      value={this.state.email}
+                      placeholder="Email"
+                      onChange={e => this.setState({ email: e.target.value })} />
+                  </div>
+                  <div className="lg:pr-4 mb-2">
+                    <input
+                      className="border rounded w-full py-2 px-3"
+                      type="text"
+                      value={this.state.username}
+                      placeholder="Username"
+                      onChange={e => this.setState({ username: e.target.value })} />
+                  </div>
+                  <button className="bg-blue-500 text-white px-4 py-2 rounded" type="submit">
+                  </button>
+                </div>
+              </form>
+            </div>
+          )}
+    )
+  }
 }
