02ab674abafda4b6e4337ccf6b1208a4aa93a7e8
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-04-28 22:55:03 -0600

    Fix erros and createUser works

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -1,24 +1,23 @@
-module Mutations
-  class CreateUser < Mutations::BaseMutations
-    argument :name, String, required: true
-    argument :email, String, required: true
-    argument :username, String, required: true
+class Mutations::CreateUser < Mutations::BaseMutation
+  argument :name, String, required: true
+  argument :email, String, required: true
+  argument :username, String, required: true
 
-    field :user, Types::UserType, null: false
-    field :errors, [String], null: false
+  field :user, Types::UserType, null: false
+  field :errors, [String], null: false
 
-    def resolve(name:, email:, username:)
-      user = User.new(name: name, email: email, username: username)
-      if user.save
-        {
-          user: user,
-          errors: []
-        }
-      else
-        {
-          user: nil,
-          errors: user.errors.full_messages
-        }
+  def resolve(name:, email:, username:)
+    user = User.new(name: name, email: email, username: username)
+    if user.save
+      {
+        user: user,
+        errors: []
+      }
+    else
+      {
+        user: nil,
+        errors: user.errors.full_messages
+      }
     end
   end
 end
